2067b0469020c6c560c40bc2acd732dad1096c26
Alex Palm <ap224gy@student.lnu.se>
2026-04-14 22:05:09 +0200

    Add routes folder and dashboard file

---
+++ src/routes/dashboard.tsx
@@ -0,0 +1,9 @@
+import { SidebarNavigation } from "../components/organisms/sidebar-navigation";
+
+export default function Dashboard() {
+    return (
+        <SidebarNavigation>
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+        </SidebarNavigation>
+    );
+}
