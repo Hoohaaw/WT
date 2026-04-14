2f13d767b8fdd8223201dfff46f72da443f15be1
Alex Palm <ap224gy@student.lnu.se>
2026-04-14 22:04:30 +0200

    Add entry point for application

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import Dashboard from "./routes/dashboard";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
+);
